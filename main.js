dc3b91b680b7f118006af1efbfc678363154c80c
insiderIns999 <152618604+insiderIns999@users.noreply.github.com>
2025-10-21 14:55:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+import { renderHeader } from './modules/renderHeader.js';
+import { renderPosts } from './modules/renderPosts.js';
+import { renderRegistrationForm } from './modules/renderRegistrationForm.js';
+import { renderAuthForm } from './modules/renderAuthForm.js';
+
+export let token;
+
+export function updateToken(newToken) {
+	token = newToken;
+}
+
+export let appEl = document.getElementById("app");
+
+renderHeader();
+renderRegistrationForm({ appEl });
+renderAuthForm({ appEl });
+renderPosts();
